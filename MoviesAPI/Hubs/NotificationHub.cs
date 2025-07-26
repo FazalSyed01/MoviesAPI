27a85dc4089ca5a1a024c086ebeb98a92aa1151b
FazalSyed01 <fazalsyed601@gmail.com>
2025-07-26 15:13:08 +0500

    Added SignalR websocket for basic live chat for now

---
+++ MoviesAPI/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MoviesAPI.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task BroadcastText(string message)
+        {
+            await Clients.All.SendAsync("ReceiveTextNotification", message);
+        }
+    }
+}
--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MoviesAPI.Data;
 using MoviesAPI.Endpoints;
+using MoviesAPI.Hubs;
+using System.Reflection;
 using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +18,25 @@ options.UseSqlServer(
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocal3000", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "http://localhost:3001")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
@@ -38,30 +54,27 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 
 });
-builder.Services.AddControllers();
+
 builder.Services.AddAuthorization();
 
-var app = builder.Build();
 
+
+var app = builder.Build();
+app.UseRewriter(new RewriteOptions().AddRedirect("^$", "/swagger"));
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = "swagger/";
+    });
+}
+
+app.UseHttpsRedirection();
 app.UseCors("AllowLocal3000");
 app.UseAuthentication();
 app.UseAuthorization();
-
-
-//Async method to seed data in the database
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<MovieContext>();
-        await context.Database.MigrateAsync();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"An error occurred while seeding the database: {ex.Message}");
-    }
-}
 app.MapControllers();
-app.MapGet("/", () => "Hello World!");
+app.MapHub<NotificationHub>("/notificationHub");
 app.Run();
